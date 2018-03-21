f909e8bb2d1dff4abe99b5c254d05aec8875e0ab
David Klein <david.klein@psmtec.de>
2018-03-21 09:29:25 +0100

    move examples to separate repo

--- Example/khafile.js
+++
@@ -1,6 +0,0 @@
-let project = new Project('slices');
-project.addAssets('Assets/**');
-project.addLibrary('kha-textureatlas-extension'); // optional
-project.addSources('..');
-project.addSources('src');
-resolve(project);
